30bead7ec3323bc96f2b3f8a8cfd4707019a0667
Pete Heyes <peter.e.heyes@gmail.com>
2023-04-02 13:06:59 +0100

    Inital setup and header styles

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+import './styles/app.scss';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  ) 
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header: React.FunctionComponent = () => {
+  return (
+    <div className='header'>
+      <select>
+        <option value="">Recommended</option>
+      </select>
+      <p>702 results</p>
+    </div>
+  )
+}
+
+export default Header
